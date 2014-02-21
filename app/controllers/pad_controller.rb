308a424b7c93515e525ab982451fe2cb612a5a92
Pankaj Bhambhani <pankajb64@gmail.com>
2014-02-21 21:57:26 +0530

    Complete remaining actions and views

--- app/controllers/pad_controller.rb
+++ app/controllers/pad_controller.rb
@@ -6,42 +6,43 @@ class PadController < ApplicationController
   
   def new
 	@pad = Pad.create!
-	@pad.base36_id = Pad.get_base36_id(pad)
+	@pad.base36_id = Pad.get_base36_id(@pad.id)
 	@pad.save!
-	redirect_to pad_get_path(:b36id => @pad.base_36_id), notice: "New Pad Created"
+	redirect_to pad_get_path(:b36id => @pad.base36_id), notice: "New Pad Created"
   end
   
   def edit
 	id = Pad.get_id(params[:b36id])
-	@pad = Pad.find(id)
+	@pad = Pad.find_by_id(id)
 
 	if @pad.nil? 
 		render :invalid_pad
+	end	
   end
   
   def save
 	j = ActiveSupport::JSON
-	@data = j.decode(params[:data], :symbolize_keys => true)
-	@pad = Pad.find_by_b36id(@data[:key])
-	if not @pad.nil? and @data.has_key?(:value)
-		@pad.contents = data[:value]
+	#@data = j.decode(params[:data], :symbolize_keys => true)
+	@pad = Pad.find_by_b36id(params[:b36id])
+	if not @pad.nil? and params.has_key?(:value)
+		@pad.contents = params[:value]
 		@pad.save!
-		flash[:notice] = "Data saved successfully."
-		render :json, => j.encode(Pad.create_successs_hash(@pad.id))
+		render :json => j.encode(Pad.create_success_hash(@pad.id))
 	else
 		render :json => j.encode(Pad.create_error_hash)
 	end
   end
   
   def delete
-	id = Pad.get_id[:b36id)
-	@pad = Pad.find(id)
+	id = Pad.get_id(params[:b36id])
+	@pad = Pad.find_by_id(id)
 	
 	if @pad.nil?
 		return :invalid_pad
 	else
-		@pad.delete!
+		@pad.delete
 		flash[:notice] = "Pad deleted successfully."
-		redirect_to root_path	
-  end
+		redirect_to action: 'index', status: 303
+	end
+  end		
 end
--- app/models/pad.rb
+++ app/models/pad.rb
@@ -1,7 +1,7 @@
 class Pad < ActiveRecord::Base
 
-def self.get_base36_id(pad)
-	return pad.id.to_s(36)
+def self.get_base36_id(id)
+	return id.to_s(36)
 end
 
 def self.get_id(base36_str)
@@ -10,4 +10,14 @@ end
 
 def self.find_by_b36id(b36id)
 	return Pad.find(self.get_id(b36id))
+end
+
+def self.create_success_hash(id)
+	return {:message => "Data saved successfully"}
+end
+
+def self.create_error_hash
+	return {:message => "Data could not be save due to an error", }
+end		
+		
 end
