0a73158fba5968bf0665be4e733e748966a3d3ba
Pankaj Bhambhani <pankajb64@gmail.com>
2014-02-21 23:01:39 +0530

    Changed db to PGSQL from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140221163515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pads", force: true do |t|
     t.string   "base36_id"
     t.text     "contents"
