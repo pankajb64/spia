cc0f9c10f427ff09273984f1a638ec2912a5e770
Pankaj Bhambhani <pankajb64@gmail.com>
2014-02-21 19:08:43 +0530

    Completed controller actions

--- app/controllers/pad_controller.rb
+++ app/controllers/pad_controller.rb
@@ -1,3 +1,5 @@
+require 'active_support'
+
 class PadController < ApplicationController
   def index
   end
@@ -14,12 +16,32 @@ class PadController < ApplicationController
 	@pad = Pad.find(id)
 
 	if @pad.nil? 
-		
+		render :invalid_pad
   end
   
   def save
+	j = ActiveSupport::JSON
+	@data = j.decode(params[:data], :symbolize_keys => true)
+	@pad = Pad.find_by_b36id(@data[:key])
+	if not @pad.nil? and @data.has_key?(:value)
+		@pad.contents = data[:value]
+		@pad.save!
+		flash[:notice] = "Data saved successfully."
+		render :json, => j.encode(Pad.create_successs_hash(@pad.id))
+	else
+		render :json => j.encode(Pad.create_error_hash)
+	end
   end
   
   def delete
+	id = Pad.get_id[:b36id)
+	@pad = Pad.find(id)
+	
+	if @pad.nil?
+		return :invalid_pad
+	else
+		@pad.delete!
+		flash[:notice] = "Pad deleted successfully."
+		redirect_to root_path	
   end
 end
--- app/models/pad.rb
+++ app/models/pad.rb
@@ -7,3 +7,7 @@ end
 def self.get_id(base36_str)
 	return base36_str.to_i(36)
 end
+
+def self.find_by_b36id(b36id)
+	return Pad.find(self.get_id(b36id))
+end
