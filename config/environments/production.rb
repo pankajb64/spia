24d64dbfe3aea573271450198dd9ba59ca49235f
Pankaj Bhambhani <pankajb64@gmail.com>
2014-02-21 23:35:00 +0530

    Modfified production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Spia::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -22,6 +22,7 @@ Spia::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  config.assets.precompile = ['*.js', , '*.js.coffee', '*.css', '*.css.scss']
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
